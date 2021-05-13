e2cdb600108f55816d7ef87f08999911bef7e445
saifaldin14 <saifaldindeveloper@gmail.com>
2021-05-12 21:14:02 -0400

    Started the Navbar

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,11 @@
-(function() {
-	"use strict";
+(function () {
+  "use strict";
 
-	// Add Scripts Here
-    	
+  // Add Scripts Here
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
 })();
