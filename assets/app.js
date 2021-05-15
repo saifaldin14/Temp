952c539ac94288a44350dec8a26d18a166b3ad01
saifaldin14 <saifaldindeveloper@gmail.com>
2021-05-15 16:30:47 -0400

    Hide and Show details button works

--- assets/app.js
+++ assets/app.js
@@ -9,10 +9,22 @@
     navbarLinks.classList.toggle("active");
   });
 
-  const toggleContentButton = document.getElementById("content-button");
+  const toggleContentButton =
+    document.getElementsByClassName("content-button")[0];
+  const toggleContentButtonHidden = document.getElementsByClassName(
+    "content-button-hidden"
+  )[0];
   const contentLinks = document.getElementsByClassName("details-div")[0];
 
   toggleContentButton.addEventListener("click", () => {
+    toggleContentButtonHidden.classList.toggle("active");
+    toggleContentButton.classList.toggle("active");
+    contentLinks.classList.toggle("active");
+  });
+
+  toggleContentButtonHidden.addEventListener("click", () => {
+    toggleContentButtonHidden.classList.toggle("active");
+    toggleContentButton.classList.toggle("active");
     contentLinks.classList.toggle("active");
   });
 })();
