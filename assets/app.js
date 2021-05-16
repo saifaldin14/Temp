cf0eeef67343bb02e06468cfe4dd1be58ce33862
saifaldin14 <saifaldindeveloper@gmail.com>
2021-05-16 18:58:55 -0400

    Added little chevron

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,20 @@
 
   // Add Scripts Here
   const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const toggleButtonHidden = document.getElementsByClassName(
+    "toggle-button-hidden"
+  )[0];
   const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
   toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+    toggleButton.classList.toggle("active");
+    toggleButtonHidden.classList.toggle("active");
+  });
+
+  toggleButtonHidden.addEventListener("click", () => {
+    toggleButtonHidden.classList.toggle("active");
+    toggleButton.classList.toggle("active");
     navbarLinks.classList.toggle("active");
   });
 
@@ -27,4 +38,8 @@
     toggleContentButton.classList.toggle("active");
     contentLinks.classList.toggle("active");
   });
+
+  function rotate(x) {
+    x.classList.toggle("fas fa-chevron-down");
+  }
 })();
