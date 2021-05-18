5328e50929343aaa11cd3107d60d781c657032a0
saifaldin14 <24593380+saifaldin14@users.noreply.github.com>
2021-05-17 21:35:03 -0400

    Initial commit

--- assets/app.js
+++ assets/app.js
@@ -1,45 +1,6 @@
-(function () {
-  "use strict";
+(function() {
+	"use strict";
 
-  // Add Scripts Here
-  const toggleButton = document.getElementsByClassName("toggle-button")[0];
-  const toggleButtonHidden = document.getElementsByClassName(
-    "toggle-button-hidden"
-  )[0];
-  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-  toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-    toggleButton.classList.toggle("active");
-    toggleButtonHidden.classList.toggle("active");
-  });
-
-  toggleButtonHidden.addEventListener("click", () => {
-    toggleButtonHidden.classList.toggle("active");
-    toggleButton.classList.toggle("active");
-    navbarLinks.classList.toggle("active");
-  });
-
-  const toggleContentButton =
-    document.getElementsByClassName("content-button")[0];
-  const toggleContentButtonHidden = document.getElementsByClassName(
-    "content-button-hidden"
-  )[0];
-  const contentLinks = document.getElementsByClassName("details-div")[0];
-
-  toggleContentButton.addEventListener("click", () => {
-    toggleContentButtonHidden.classList.toggle("active");
-    toggleContentButton.classList.toggle("active");
-    contentLinks.classList.toggle("active");
-  });
-
-  toggleContentButtonHidden.addEventListener("click", () => {
-    toggleContentButtonHidden.classList.toggle("active");
-    toggleContentButton.classList.toggle("active");
-    contentLinks.classList.toggle("active");
-  });
-
-  function rotate(x) {
-    x.classList.toggle("fas fa-chevron-down");
-  }
+	// Add Scripts Here
+    	
 })();
